c0e1b1f07214c709af94002035189d303bf5b8a0
AndreiArtsiomenko <andartsiomenko@gmail.com>
2022-11-08 02:21:04 +0300

    feat: add solution of task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,29 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+(async function () {
+  try {
+    const pathFolder = path.join(__dirname, 'files');
+    const creatFolder = fs.mkdir(path.join(__dirname, 'files-copy'), {
+      recursive: true
+    });
+    const pathFolderCopy = path.join(__dirname, 'files-copy');
+
+    const filesCopy = await fs.readdir(pathFolderCopy);
+
+    for (let file of filesCopy) {
+      fs.unlink(path.join(pathFolderCopy, file));
+    }
+
+    const files = await fs.readdir(pathFolder, {
+      withFileTypes: true
+    });
+
+    for (let file of files) {
+      fs.copyFile(path.join(pathFolder, file.name), path.join(pathFolderCopy, file.name));
+    }
+
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+})();
