9cdd39a25a19915a762ddad488a33556b6ef2451
AndreiArtsiomenko <andartsiomenko@gmail.com>
2022-11-08 21:40:21 +0300

    feat: add the entry of kb

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,9 +1,10 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-(async function () {
+async function countFiles () {
   try {
     const pathFolder = path.join(__dirname, 'secret-folder');
+   
     const files = await fs.readdir(pathFolder, {
       withFileTypes: true
     });
@@ -14,11 +15,13 @@ const path = require('path');
         const fileName = path.basename(file.name).split('.')[0];
         const fileExt = path.extname(file.name).slice(1);
         const fileSize = fileOn.size;
-        console.log(`${fileName} - ${fileExt} - ${fileSize}`);
+        console.log(`${fileName} - ${fileExt} - ${fileSize}b / ${(fileSize/1024).toFixed(3)}kb`);
       }
     }
 
   } catch (error) {
     console.error('Error:', error.message);
   }
-})();
+};
+
+countFiles()
