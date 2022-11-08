f7a7e530a973645398bced66151c9b446477a0de
AndreiArtsiomenko <andartsiomenko@gmail.com>
2022-11-08 23:54:00 +0300

    fix: change solution of task 05

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,1 +1,26 @@
-console.log('Добрый день, проверяющий. Прошу проверить мою работу в среду вечером. Надеюсь, это не затруднит тебя. И не будь student 1),  оставь свой Discord. Хорошего дня.')
+const fs = require('fs');
+const path = require('path');
+
+const stylePath = path.join(__dirname, 'styles')
+const bundleStream = fs.createWriteStream(path.join(__dirname, 'project-dist','bundle.css'));
+
+fs.readdir(stylePath, { withFileTypes: true }, (err, files) => {
+  
+  if (err) {
+    console.error(err)
+    return
+  }
+
+  files.forEach(file => {
+
+    const fileExt = path.extname(file.name).slice(1);
+
+    if (file.isFile() && fileExt === 'css') {
+      const fileStream = fs.createReadStream(path.join(stylePath, file.name), 'utf-8');
+      fileStream.pipe(bundleStream)
+    }
+
+  })
+});
+
+
